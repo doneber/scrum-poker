8a3adfd08f808a7304919ba71c102af02b9868eb
doneber <doneber98@gmail.com>
2021-02-04 14:30:43 -0400

    Initialize base backend project

---
+++ backend/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'app.ts'),
+  mode: 'production',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  }
+}
